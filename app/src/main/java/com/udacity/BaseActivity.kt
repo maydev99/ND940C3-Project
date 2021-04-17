b8bbd99c28803ed67038f0d9e8bd07b3e2847c4e
michael <maydev99@gmail.com>
2021-04-17 16:09:58 -0400

    Added Internet Connect Check

---
+++ app/src/main/java/com/udacity/BaseActivity.kt
@@ -0,0 +1,57 @@
+package com.udacity
+
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+open class BaseActivity : AppCompatActivity() {
+
+    /**
+     * Checks if App is Open so that the Download Toast
+     * in the MainActivity does not show if app is closed
+     */
+
+    var isAppInForeground = false
+
+    override fun onResume() {
+        super.onResume()
+        isAppInForeground = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isAppInForeground = false
+    }
+
+    //Check Internet Utility
+
+    fun isConnected(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+}
